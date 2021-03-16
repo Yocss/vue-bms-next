da589c7e6a4704899457bea9572ad4bd741161d4
Rabbit <rabbit@RabbitdeiMac.local>
2021-03-17 00:34:21 +0800

    add base style

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from '@/layouts/App.vue'
 import router from './router'
 import store from './store'
-import 'ant-design-vue/dist/antd.css'
+// import 'ant-design-vue/dist/antd.css'
+import '@/assets/css/index.styl'
 
 createApp(App).use(store).use(router).mount('#app')
