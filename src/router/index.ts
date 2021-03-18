751cfee999155cdcfc4ef6b449f4d0daa320e12c
Rabbit <rabbit@RabbitdeiMac.local>
2021-03-19 00:59:22 +0800

    how to show icon at menu in side bar?

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,37 +10,37 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     name: 'Home',
     component: Home,
-    meta: { title: '控制台首页', icon: 'dashboard' }
+    meta: { title: '控制台首页', icon: 'DashboardOutlined' }
   },
   {
     path: '/article',
     name: 'ArticleIndex',
     component: () => import('@/pages/article/index.vue'),
-    meta: { title: '文章管理', icon: '' }
+    meta: { title: '文章管理', icon: 'CoffeeOutlined' }
   },
   {
     path: '/ads',
     name: 'AdsIndex',
     component: () => import('@/pages/ads/index.vue'),
-    meta: { title: '广告管理', icon: '' }
+    meta: { title: '广告管理', icon: 'AppstoreOutlined' }
   },
   {
     path: '/system',
     name: 'SystemIndex',
     component: () => import('@/pages/system/index.vue'),
-    meta: { title: '系统管理', icon: '' }
+    meta: { title: '系统管理', icon: 'SettingOutlined' }
   },
   {
     path: '/account',
     name: 'AccountIndex',
     component: () => import('@/pages/account/index.vue'),
-    meta: { title: '管理系统登录', layout: 'null' }
+    meta: { title: '管理系统登录', layout: 'null', icon: 'AppstoreOutlined' }
   },
   {
     path: '/:pathMatch(.*)*',
     name: 'ErrorIndex',
     component: () => import('@/pages/error/index.vue'),
-    meta: { title: '访问出错了', layout: 'null' }
+    meta: { title: '访问出错了', layout: 'null', icon: 'AppstoreOutlined' }
   }
 ]
 
