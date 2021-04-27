6b5c3a7275e35cd3983e80770f7082ba4b499c36
Axmine <yeahoor@qq.com>
2021-04-27 18:02:25 +0800

    按钮未搞定

---
+++ src/components/common/com-table/plugin-btns.vue
@@ -0,0 +1,21 @@
+<template>
+  <a-popover
+    placement="left"
+  >
+    <template v-slot:content>
+      <div class="com-table-action-btns flex-center"></div>
+    </template>
+  </a-popover>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Button, Popover } from 'ant-design-vue'
+
+export default defineComponent({
+  name: 'ComTablePluginsBtns',
+  components: {
+    [Button.name]: Button,
+    [Popover.name]: Popover
+  }
+})
+</script>
